06b5c2db327508b2639be53c010d1d7ea29901f2
mazeneltayeb <127837619+mazeneltayeb@users.noreply.github.com>
2024-06-29 20:06:38 +0300

    Frist Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// var element = document.querySelector('.map');
+// var styles = window.getComputedStyle(element,':after')
+// styles.style.color="red"
+// let m =window.getComputedStyle(
+//     document.querySelector('.map'), ':after'
+// );
+// m.style.color="red"
+// let map = document.querySelector('.map');
+// let discription = document.querySelector('.discription');
+
+// map.addEventListener("hover",()=>{
+//     // map.style.opacity="0"
+//     discription.style.opacity="0"
+// })
+
+// map.onHover=()=>{
+//     discription.style.opacity="0"
+// }
+
+let button = document.querySelector('button');
+console.log(button)
+button.onclick=()=>{
+location.href="https://maps.google.com/maps?q=30.585680,32.287534&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+}
